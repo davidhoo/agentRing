3f4aea0af872b543f969b6616cfae756d266adb5
HaoRui <haorui@staff.weibo.com>
2026-09-16 15:24:02 +0800

    降低 Dashboard 圆环已用部分半透明透明度
    已用底轨透明度从 18% 调至 12%，视觉更淡更不抢主弧。

--- AgentRing/Views/Components/ActivityRingView.swift
+++ AgentRing/Views/Components/ActivityRingView.swift
@@ -59,7 +59,7 @@ struct ActivityRingView: View {
         .frame(width: diameter, height: diameter)
     }
 
-    private let usedPortionOpacity: Double = 0.18
+    private let usedPortionOpacity: Double = 0.12
 
     @ViewBuilder
     private func ring(diameter: CGFloat, percentage: Double, color: Color, isInner: Bool) -> some View {
